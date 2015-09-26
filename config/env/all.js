96866ee06131b1395b8d373d3a846fb12de40bd6
Joanclopez <joanclopezm@gmail.com>
2015-09-26 10:19:59 -0600

    fix createjs

--- config/env/all.js
+++ config/env/all.js
@@ -26,7 +26,7 @@ module.exports = {
 				'public/lib/angular-ui-router/release/angular-ui-router.js',
 				'public/lib/angular-ui-utils/ui-utils.js',
 				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
-				'public/lib/createjs.js'
+				'public/createjs.js'
 			]
 		},
 		css: [
